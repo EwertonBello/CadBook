523b3129c81d846d9396c42fef67e3b5f8f5e18e
EwertonBello <ewerton.bello1212@gmail.com>
2020-10-05 00:51:45 -0300

    helper asset to secure_asset for deploy https

--- resources/views/layouts/book.blade.php
+++ resources/views/layouts/book.blade.php
@@ -11,14 +11,14 @@
     <title>Cad-Books - Crie títulos imaginários</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
 
 
 </head>
@@ -101,27 +101,27 @@
 </a>
 
 <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+<script src="{{ secure_asset('js/sb-admin-2.min.js') }}"></script>
 
 {{-- Book scripts --}}
 
 {{-- deleteBook Ajax Delete --}}
-<script src="{{ asset('js/book/book-delete.js') }}"></script>
+<script src="{{ secure_asset('js/book/book-delete.js') }}"></script>
 {{-- datatable list of books --}}
-<script src="{{ asset('js/book/book-datatable.js') }}"></script>
+<script src="{{ secure_asset('js/book/book-datatable.js') }}"></script>
 {{-- Book scripts --}}
 
 
 <!-- Page level plugins -->
-<script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ secure_asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ secure_asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
 {{-- Alert delete book --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
